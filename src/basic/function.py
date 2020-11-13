078c5e6203008c21fe3cc1bcd8e04e3987ae4c2f
imarklei90 <imarklei90@126.com>
2020-11-13 16:46:41 +0800

    commit function

---
+++ src/basic/function.py
@@ -0,0 +1,22 @@
+# coding:utf-8
+
+var1 = 2
+
+def method(str):
+    """
+        这是一个简单的方法
+    :return:
+    """
+    print(str)
+    global var1
+    print(var1 + 1)
+    return str + "AAAA"
+
+# lambda函数
+sum = lambda arg1, arg2: arg1 + arg2
+
+if __name__ == '__main__':
+    retValue = method("python")
+    print(retValue)
+    result = sum(1,2)
+    print(result)
