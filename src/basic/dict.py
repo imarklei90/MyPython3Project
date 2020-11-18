a13fa1a48742468eb6ac13e374ca01373fc056f4
imarklei90 <imarklei90@126.com>
2020-11-18 09:30:40 +0800

    [dev] add dict demo

--- src/basic/dict.py
+++ src/basic/dict.py
@@ -2,20 +2,28 @@
 
 dict = {'name': 'zhangsan', 'age': 123}
 
-print(dict)
+#print(dict)
 
-print(dict['name'])
+#print(dict['name'])
 
 # 修改
 dict['name'] = 'lisi'
 dict['age'] = 2
 
-print(dict)
+#print(dict)
 
 # 删除
-del dict['age']
-print(dict)
+#del dict['age']
+#print(dict)
 
 # 清空
-dict.clear()
-print(dict)
+#dict.clear()
+#print(dict)
+
+# 输出key
+for i in dict:
+    print(i)
+
+# 同时输出key、value
+for k, v in dict.items():
+    print(f'{k, v}')
