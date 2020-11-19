be0ad6485d0de717ad819fb448e9706a0ec4b67a
imarklei90 <imarklei90@126.com>
2020-11-19 09:46:58 +0800

    [dev] add function with return value type

--- src/basic/function.py
+++ src/basic/function.py
@@ -2,6 +2,7 @@
 
 var1 = 2
 
+
 def method(str):
     """
         这是一个简单的方法
@@ -16,8 +17,20 @@ def method(str):
 # lambda函数
 sum = lambda arg1, arg2: arg1 + arg2
 
+
+# 定义带有返回值类型的函数
+def method2(s: str) -> str:
+    return s
+
+
+
 if __name__ == '__main__':
     retValue = method("python")
     print(retValue)
     result = sum(1,2)
     print(result)
+
+    # 调用带有返回值类型的函数
+    result2 = method2('aaa')
+    # 可以调用str类型的所有方法
+    print(result2.upper())
